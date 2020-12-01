095238a6593c0cff402f4ed1fc68b117f1a18f80
mickname <mikko.luodemaa@mikkis.net>
2020-12-01 17:33:40 +0200

    Day 1

---
+++ day1/part12.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import functools
+import itertools
+import operator
+import sys
+
+
+def main():
+    try:
+        _, fname, num_numbers = sys.argv
+        num_numbers = int(num_numbers)
+    except (ValueError, IndexError):
+        print(f"Usage: {sys.argv[0]} input.txt NUMBER_OF_ENTRIES_TO_SUM", file=sys.stderr)
+        return
+
+    with open(sys.argv[1]) as f:
+        iter_numbers = (int(line) for line in f)
+        for numbers in itertools.combinations(iter_numbers, num_numbers):
+            if sum(numbers) == 2020:
+                product = functools.reduce(operator.mul, numbers)
+                print(f"Entries: {' + '.join((str(num) for num in numbers))} == 2020")
+                print(f"Answer: {product}")
+                return
+
+
+if __name__ == '__main__':
+    main()
